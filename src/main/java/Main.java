af3ecd3b9508eaf5abbe7f371109d03906156f3f
Dheeraj Bisht <dheerajbisht876@gmail.com>
2023-11-03 17:37:20 +0530

    hello window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import engine.Window;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Mario");
+        Window window = Window.get();
+        window.run();
     }
 }
