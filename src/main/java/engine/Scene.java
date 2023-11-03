8ff1c694f434576c95b32630a13e0b8032f350c3
Dheeraj Bisht <dheerajbisht876@gmail.com>
2023-11-03 17:39:19 +0530

    scene and listeners added to window

---
+++ src/main/java/engine/Scene.java
@@ -0,0 +1,11 @@
+package engine;
+
+public abstract class Scene {
+
+    public Scene() {
+
+    }
+
+    public abstract void update(float dt);
+
+}
--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -3,6 +3,7 @@ package engine;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -17,6 +18,7 @@ public class Window {
 
     private static Window window = null;
 
+    private static Scene currentScene;
     private Window() {
         this.width = 1920;
         this.height = 1080;
@@ -34,6 +36,20 @@ public class Window {
         }
 
         return Window.window;
+    }
+
+    public static void changeScene(int newScene) {
+        switch(newScene) {
+            case 0:
+                currentScene = new LevelEditorScene();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                break;
+            default:
+                assert false : "Unknown scene '"+ newScene +"'";
+                break;
+        }
     }
 
     public void run() {
@@ -72,6 +88,11 @@ public class Window {
             throw new IllegalStateException("Failed to create GLFW window");
         }
 
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
         // Enable v-sync
@@ -86,9 +107,14 @@ public class Window {
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop() {
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+        float dt = -1.0f;
 
         while(!glfwWindowShouldClose(glfwWindow)) {
             // Poll Events
@@ -97,8 +123,15 @@ public class Window {
             glClearColor(r,g,b,a);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            if(dt >= 0) {
+                currentScene.update(dt);
+            }
+
             glfwSwapBuffers(glfwWindow);
 
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 }
